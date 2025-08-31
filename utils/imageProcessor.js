2ba469e554541fb0e92420749e310c44f398574d
Jack Graw <129253326+n11040459@users.noreply.github.com>
2025-08-31 18:38:35 +1000

    Update imageProcessor.js

--- utils/imageProcessor.js
+++ utils/imageProcessor.js
@@ -1,1 +1,7 @@
+const sharp = require('sharp');
 
+exports.resizeImage = async (inputPath, outputPath, width, height) => {
+  await sharp(inputPath)
+    .resize(width, height)
+    .toFile(outputPath);
+};
