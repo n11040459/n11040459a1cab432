495cb091edf9b835caf89dcbe4d40a9868cf495b
Jack Graw <129253326+n11040459@users.noreply.github.com>
2025-08-31 18:38:50 +1000

    Update processController.js

--- controllers/processController.js
+++ controllers/processController.js
@@ -1,1 +1,14 @@
+const multer = require('multer');
+const path = require('path');
+const { resizeImage } = require('../utils/imageProcessor');
 
+const upload = multer({ dest: 'uploads/' });
+
+exports.uploadMiddleware = upload.single('image');
+
+exports.processImage = async (req, res) => {
+  const inputPath = req.file.path;
+  const outputPath = path.join('uploads', `resized-${req.file.filename}.jpg`);
+  await resizeImage(inputPath, outputPath, 300, 300);
+  res.json({ message: 'Image processed', output: outputPath });
+};
