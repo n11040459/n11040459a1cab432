3138010e326a0b4ec1e49448a9afb23bc669f79b
Jack Graw <129253326+n11040459@users.noreply.github.com>
2025-08-31 18:37:41 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/auth');
+const processRoutes = require('./routes/process');
 
+app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/process', processRoutes);
+
+app.listen(3000, () => console.log('Server running on port 3000'));
